98efd6904c0554b7b08450970a23cd46ce6e1f1b
abdurrahim bulut <abdurrahim.bulut@tsoft.com.tr>
2022-11-13 23:21:15 +0300

    PHP unit installed & Member class

---
+++ src/Member.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace User\PhpTddUnitTest;
+
+class Member
+{
+    private $members =[
+        [
+            "name"=>"Abdurrahim Bulut",
+            "age"=>24
+        ],
+        [
+            "name"=>"Lionel Messi",
+            "age"=>35
+        ],
+        [
+            "name"=>"Cristiano Ronaldo",
+            "age"=>37
+        ]
+    ];
+
+
+    /**
+     * @return array[]
+     */
+    public function getMembers()
+    {
+        return $this->members;
+    }
+
+    /**
+     * @return integer
+     * */
+    public function sumMemberAge()
+    {
+        $totalAge = 0;
+        foreach ($this->members as $member){
+            $totalAge += $member["age"];
+        }
+        return $totalAge;
+    }
+
+}
